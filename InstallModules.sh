e97569e9485ba6c4d62fb5b5900407ff9adfe1d8
97hackbrian <brayandurantoconas@gmail.com>
2023-06-25 02:23:48 -0400

    InstallDependenciasV0

--- InstallModules.sh
+++ InstallModules.sh
@@ -1,2 +1,3 @@
 echo "instalando modulos y dependencias *asegurar de estar en un entorno virtual*"
-pip install -r requirements.txt
+pip3 install --force-reinstall -r requirements.txt
+pip3 install pyqt6-qt6==6.4.2
